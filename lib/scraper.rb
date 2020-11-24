994e743b9a54cca614b61758368e7eeaca99f3d9
cep104 <candicelizabethpeters@gmail.com>
2020-11-23 20:16:41 -0600

    updated with correction

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+# require 'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+ doc.css(".gridContainer-EnvccM").each do |course|
+#  puts course.text
+ end
+ 
+ p doc.css(".gridContainer-EnvccM")[0].children.text
+# binding.pry
